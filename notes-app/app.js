d89d7a1ee0fa2b96efc49ddaf6c59529a9adf3ab
Christian Tisby <cduane82@hotmail.com>
2020-02-22 17:11:17 -0500

    adding notes app

---
+++ notes-app/app.js
@@ -0,0 +1,18 @@
+
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+console.log(msg)
+
+
+
+
+
+
+// const add =  require('./utils.js')
+
+// const sum = add(4, -2)
+// console.log(sum)
+
+
+
---
+++ notes-app/utils.js
@@ -0,0 +1,8 @@
+//console.log('utils.js')
+
+const name = 'Reggie'
+const add = function(a, b) {
+    return a + b
+}
+
+module.exports = add
