645e2adf0c6aa39b2c7675279c6564ba5e8f0ee8
Christian Tisby <cduane82@hotmail.com>
2020-02-22 20:59:10 -0500

    adding notes app updates

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,11 @@
-
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const msg = getNotes()
 console.log(msg)
 
 
-
-
+console.log(chalk.green.inverse.bold('Success!'));
 
 
 // const add =  require('./utils.js')
